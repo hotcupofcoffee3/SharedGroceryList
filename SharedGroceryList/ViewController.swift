5cb288449bdf7952a753333103d8e1bd9170825d
Adam Moore <adammoore@adams-macbook-pro.local>
2018-06-18 15:43:38 -0600

    Initial commit.

--- SharedGroceryList/ViewController.swift
+++ SharedGroceryList/ViewController.swift
@@ -7,8 +7,63 @@
 //
 
 import UIKit
+import CoreData
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func add(newItem: String) {
+        
+        groceryList.append(newItem)
+        
+    }
+    
+    @IBAction func addGroceryItem(_ sender: UIButton) {
+        
+        if addItemTextField.text != "" {
+            
+            add(newItem: addItemTextField.text!)
+            
+            addItemTextField.text = ""
+            
+            tableOfItems.reloadData()
+            
+        }
+        
+    }
+    
+    @IBOutlet weak var tableOfItems: UITableView!
+    
+    @IBOutlet weak var addItemTextField: UITextField!
+    
+    var groceryList = [String]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groceryList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = groceryList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            groceryList.remove(at: indexPath.row)
+            
+            tableView.reloadData()
+            
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
